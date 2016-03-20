ad786a42ca5476d0b5d91bcdd433282b6dbe52a7
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-03-20 19:09:19 +0800

    retreat .

--- is_balanced_Tree.rb
+++ is_balanced_Tree.rb
@@ -1,9 +1,12 @@
 def assert(tree)
   return 0 if not tree
-  left, right = assert(tree.left), assert(tree.right)
-  (left - right).abs > 1 ? raise : 1 + [left, right].max
+  left, right = check(tree.left), check(tree.right)
+  raise if (left - right).abs > 1 # unbalanced
+  1 + [left, right].max # height
 end
 
 def is_balanced(root)
-  assert(root) && true rescue false
+  check(root) && true
+rescue
+  false
 end
